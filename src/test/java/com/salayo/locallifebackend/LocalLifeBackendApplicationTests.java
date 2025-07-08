9a6351ad70b67fe9ecb2673fa761c964090492eb
jiyoon <rlawldbs9128@gmail.com>
2025-07-08 17:11:09 +0900

    :tada: begin : 프로젝트 생성 및 세팅

---
+++ src/test/java/com/salayo/locallifebackend/LocalLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salayo.locallifebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalLifeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
