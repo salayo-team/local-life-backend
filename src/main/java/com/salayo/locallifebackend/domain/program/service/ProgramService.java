da1c22fa88801cb2b652667cb1a8b90a9e1e0dc6
j-hann <86395588+j-hann@users.noreply.github.com>
2025-07-11 01:25:35 +0900

    ✨  feat : program, category entity 생성, status enum 생성
    - program entity 생성
    - AptitudeCategory & RegionCategory entity 생성
    - ProgramStatus enum 생성
    - global에 DeletedStatus enum 생성

---
+++ src/main/java/com/salayo/locallifebackend/domain/program/service/ProgramService.java
@@ -0,0 +1,8 @@
+package com.salayo.locallifebackend.domain.program.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgramService {
+
+}
---
+++ src/main/java/com/salayo/locallifebackend/global/enums/DeletedStatus.java
@@ -0,0 +1,7 @@
+package com.salayo.locallifebackend.global.enums;
+
+public enum DeletedStatus {
+	DISPLAYED, //게시
+	DELETED, //삭제
+	;
+}
