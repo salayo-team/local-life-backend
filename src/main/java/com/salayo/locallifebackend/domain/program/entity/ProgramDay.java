23aab85a039f56c29d3b5d8e28eb62b5b895f684
j-hann <86395588+j-hann@users.noreply.github.com>
2025-07-19 23:12:58 +0900

    ✨ feat : 체험 프로그램 생성 API 구현 및 도메인 구조 변경
    🟪 Program, ProgramDay, ProgramScheduleTime 코드 리팩토링
    - JPA가 programId FK를 자동 주입하도록 개선
    
    🟪 Program, AptitudeCategory, RegionCategory 관계 변경
    - OneToOne에서 ManyToOne으로 변경
    - 하나의 적성 & 지역 카테고리가 0개 또는 N개의 체험 프로그램을 가질 수 있게 재설정
    
    🟪 AptitudeCategoryRepository , RegionCategoryRepository, ProgramDayResponseDto, ProgramScheduleTimeResponseDto, ProgramDayRepository, ProgramScheduleTimeRepository 생성
    
    🟪 RequestDto @Builder 제거 후 @NoArgsConstructor 추가
    
    🟪 ProgramController 체험 프로그램 생성 API 구현
    - @AuthenticationPrincipal 활용해 JWT 토큰에서 사용자 정보 추출 로직 추가
    
    🟪 ProgramCreateResponseDto에 from() 정적 메서드 추가
    - programDays, programScheduleTimes 데이터 포함되도록 추가
    
    🟪 ProgramService createProgram 메서드 로직 구현
    - memberId 기반 Member 조회 및 승인된 로컬 크리에이터 체크
    - 적성 & 지역 카테고리 id 기반 조회 및 검증
    - 가격 및 할인율 검증, 할인 가격 계산 로직 추가 & 최소 & 최대 정원 검증 로직 추가
    - 시작일 & 종료일 날짜 범위 검증 메서드 분리
    - Program Entity 생성 & 상태값 기본 설정
    - ProgramDay, ProgramScheduleTime Entity 생성 및 매핑 로직 추가
    - 시작 시간 + 소요 시간 통해서 종료 시간 자동 계산
    - ProgramCreateResponseDto.from()으로 응답 생성
    
    🟪 LocalCreatorRepository, MemberRepository 메서드 추가
    - 중복되는 .orElseThrow() 로직을 방지하기 위해 공통 메서드로 추출
    
    🟪 ErrorCode 추가
    
    🟪 Program.http 체험 프로그램 등록 API 호출 스크립트 작성
    - 로그인 시 accessToken을 전역 변수로 저장하도록 스크립트 코드 추가
    
    🟪 Swagger @SecurityRequirement(name = "Bearer Authentication") 설정 추가
    
    🟪 ProgramCreateRequestDto title, description, curriculumDescription, location 필드 @NotNull로 변경
    
    🟪 ProgramService 불필요한 주석 삭제
    
    🟪 AptitudeCategory Entity 적성 코드 필드 추가
    
    🟪 Program Entity discountPrice 필드 finalPrice로 필드명 변경 & ProgramService createProgram() 로직 수정
    - 할인 없으면 price 값 finalPrice으로 넣어주는 로직으로 변경

--- src/main/java/com/salayo/locallifebackend/domain/program/entity/ProgramDay.java
+++ src/main/java/com/salayo/locallifebackend/domain/program/entity/ProgramDay.java
@@ -5,12 +5,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -25,19 +22,16 @@ public class ProgramDay {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; //체험 프로그램 고유 식별자
+	private Long id; //체험 프로그램 요일 고유 식별자
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "day_name", nullable = false, length = 50)
 	private DayName dayName; //요일명
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "program_id", nullable = false)
-	private Program program;
-
 	@Builder
-	public ProgramDay(DayName dayName, Program program) {
+	public ProgramDay(DayName dayName) {
 		this.dayName = dayName;
-		this.program = program;
+
 	}
+
 }
--- src/main/java/com/salayo/locallifebackend/domain/program/service/ProgramService.java
+++ src/main/java/com/salayo/locallifebackend/domain/program/service/ProgramService.java
@@ -1,15 +1,31 @@
 package com.salayo.locallifebackend.domain.program.service;
 
 
+import com.salayo.locallifebackend.domain.category.entity.AptitudeCategory;
+import com.salayo.locallifebackend.domain.category.entity.RegionCategory;
 import com.salayo.locallifebackend.domain.category.repository.AptitudeCategoryRepository;
 import com.salayo.locallifebackend.domain.category.repository.RegionCategoryRepository;
 import com.salayo.locallifebackend.domain.file.repository.FileRepository;
+import com.salayo.locallifebackend.domain.localcreator.entity.LocalCreator;
+import com.salayo.locallifebackend.domain.localcreator.enums.CreatorStatus;
 import com.salayo.locallifebackend.domain.localcreator.repository.LocalCreatorRepository;
+import com.salayo.locallifebackend.domain.member.entity.Member;
 import com.salayo.locallifebackend.domain.member.repository.MemberRepository;
 import com.salayo.locallifebackend.domain.program.dto.ProgramCreateRequestDto;
 import com.salayo.locallifebackend.domain.program.dto.ProgramCreateResponseDto;
+import com.salayo.locallifebackend.domain.program.entity.Program;
+import com.salayo.locallifebackend.domain.program.entity.ProgramDay;
+import com.salayo.locallifebackend.domain.program.entity.ProgramScheduleTime;
+import com.salayo.locallifebackend.domain.program.enums.LocalSpecialized;
+import com.salayo.locallifebackend.domain.program.enums.ProgramStatus;
 import com.salayo.locallifebackend.domain.program.repository.ProgramRepository;
+import com.salayo.locallifebackend.global.enums.DeletedStatus;
+import com.salayo.locallifebackend.global.error.ErrorCode;
+import com.salayo.locallifebackend.global.error.exception.CustomException;
 import jakarta.validation.Valid;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.springframework.stereotype.Service;
 
 
@@ -36,11 +52,129 @@ public class ProgramService {
 
 	/**
 	 * 체험 프로그램 생성 메서드
-	 * - TODO : 구현 예정
+	 * - TODO : file 로직 추가
+	 * - TODO : 동일한 유저가 중복되는 스케줄 타임 생성시 예외처리
+	 * - TODO : 예외처리 이후, 스케줄 종료시간 - 마지막 스케줄 시간을 설정할때 소요시간을 더 했을때 하루 넘어가면 안되도록 예외처리
 	 */
-	public ProgramCreateResponseDto createProgram(long l, @Valid ProgramCreateRequestDto requestDto) {
+	public ProgramCreateResponseDto createProgram(long memberId, @Valid ProgramCreateRequestDto requestDto) {
 
-		return null;
+		Member member = memberRepository.findByIdOrElseThrow(memberId);
+
+		LocalCreator localCreator = localCreatorRepository
+			.findByMemberIdAndCreatorStatus(member.getId(), CreatorStatus.APPROVED)
+			.orElseThrow(() -> new CustomException(ErrorCode.LOCAL_CREATOR_NOT_APPROVED));
+
+		String businessName = localCreator.getBusinessName();
+
+		Long aptitudeCategoryId = requestDto.getAptitudeCategoryId();
+		AptitudeCategory aptitudeCategory = aptitudeCategoryRepository.findByIdOrElseThrow(aptitudeCategoryId);
+
+		Long regionCategoryId = requestDto.getRegionCategoryId();
+		RegionCategory regionCategory = regionCategoryRepository.findByIdOrElseThrow(regionCategoryId);
+
+		BigDecimal price = requestDto.getPrice();
+		BigDecimal percent = requestDto.getPercent();
+
+		if (price.compareTo(BigDecimal.ONE) < 0 || price.compareTo(new BigDecimal("5000000")) > 0) {
+			throw new CustomException(ErrorCode.INVALID_PRICE_RANGE);
+		}
+
+		BigDecimal finalPrice = price;
+
+		if (percent != null) {
+			BigDecimal discountRate = BigDecimal.ONE.subtract(percent.divide(BigDecimal.valueOf(100)));
+			finalPrice = price.multiply(discountRate);
+		}
+
+		Integer minCapacity = requestDto.getMinCapacity();
+		Integer maxCapacity = requestDto.getMaxCapacity();
+
+		if (minCapacity < 1 || maxCapacity > 100 || minCapacity > maxCapacity) {
+			throw new CustomException(ErrorCode.INVALID_CAPACITY_RANGE);
+		}
+
+		LocalDate startDate = requestDto.getStartDate();
+		validateStartDate(startDate);
+
+		LocalDate endDate = requestDto.getEndDate();
+		validateEndDate(startDate, endDate);
+
+		Program program = Program.builder()
+			.member(member)
+			.aptitudeCategory(aptitudeCategory)
+			.regionCategory(regionCategory)
+			.businessName(businessName)
+			.title(requestDto.getTitle())
+			.description(requestDto.getDescription())
+			.curriculumDescription(requestDto.getCurriculumDescription())
+			.location(requestDto.getLocation())
+			.price(price)
+			.percent(percent)
+			.finalPrice(finalPrice)
+			.minCapacity(minCapacity)
+			.maxCapacity(maxCapacity)
+			.startDate(startDate)
+			.endDate(endDate)
+			.count(0)
+			.isLocalSpecialized(LocalSpecialized.GENERAL)
+			.programStatus(ProgramStatus.REGISTERED)
+			.deletedStatus(DeletedStatus.DISPLAYED)
+			.build();
+
+		requestDto.getProgramDays().forEach(dayName -> {
+			ProgramDay programDay = ProgramDay.builder()
+				.dayName(dayName)
+				.build();
+			program.addProgramDay(programDay);
+		});
+
+		requestDto.getProgramScheduleTimes().forEach(scheduleDto -> {
+			Integer scheduleDurationHours = scheduleDto.getScheduleDuration();
+			LocalTime startTime = scheduleDto.getStartTime();
+			LocalTime endTime = startTime.plusHours(scheduleDurationHours);
+
+			ProgramScheduleTime scheduleTime = ProgramScheduleTime.builder()
+				.scheduleCount(scheduleDto.getScheduleCount())
+				.scheduleDuration(scheduleDurationHours)
+				.startTime(startTime)
+				.endTime(endTime)
+				.build();
+			program.addProgramScheduleTime(scheduleTime);
+		});
+
+		programRepository.save(program);
+
+		//TODO : 체험 프로그램 스케줄 생성 및 저장
+
+		return ProgramCreateResponseDto.from(program);
+	}
+
+	/**
+	 * 시작일 검증 메서드
+	 * - 시작일 현재 기준 7일 이후, max 1달 이내 검증
+	 */
+	private void validateStartDate(LocalDate startDate) {
+
+		LocalDate today = LocalDate.now();
+		if (startDate.isBefore(today.plusDays(7))) {
+			throw new CustomException(ErrorCode.INVALID_START_DATE);
+		}
+		if (startDate.isAfter(today.plusMonths(1))) {
+			throw new CustomException(ErrorCode.INVALID_START_DATE);
+		}
+	}
+
+	/**
+	 * 종료일 검증 메서드
+	 * - 종료일 시작일 기준 1주일 후, max 3달 이내 검증
+	 */
+	private void validateEndDate(LocalDate startDate, LocalDate endDate) {
+		if (endDate.isBefore(startDate.plusWeeks(1))) {
+			throw new CustomException(ErrorCode.INVALID_END_DATE);
+		}
+		if (endDate.isAfter(startDate.plusMonths(3))) {
+			throw new CustomException(ErrorCode.INVALID_END_DATE);
+		}
 	}
 
 }
