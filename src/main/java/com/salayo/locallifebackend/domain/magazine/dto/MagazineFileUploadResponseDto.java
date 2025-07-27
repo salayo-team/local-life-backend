8a82af91cb8c26cec42572325b3fb8b65af6947b
jiyoon <rlawldbs9128@gmail.com>
2025-07-28 01:42:36 +0900

    :sparkles: feat : 매거진 파일 업로드 구현 -매거진 썸네일/상세이미지 파일 다중 업로드 -썸네일(THUMBNAIL) 업로드 시 1개만 허용하는 예외처리 추가 -S3Uploader 연동 및 파일/파일매핑 엔티티 저장 로직 구현 -파일 목적별 업로드 분기 처리(THUMBNAIL/DETAIL_IMAGE) -임시 업로드(referenceId=0L) 구조 적용, 실제 매거진 등록 시(사용자에게 노출) 매핑 예정

---
+++ src/main/java/com/salayo/locallifebackend/domain/magazine/dto/MagazineFileUploadResponseDto.java
@@ -0,0 +1,12 @@
+package com.salayo.locallifebackend.domain.magazine.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MagazineFileUploadResponseDto {
+
+    private String fileUrl;
+
+}
