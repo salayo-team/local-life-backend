23b87e43b604c9920e64f1cf5b7390b65ffcb227
jiyoon <rlawldbs9128@gmail.com>
2025-07-28 02:25:24 +0900

    :wrench: 회원가입 이메일 인증 체크 추가 및 지역명 2글자 통일 -일반회원 및 로컬크리에이터 회원가입 시 이메일 인증 플래그(email_verified) 체크 -지역 카테고리 데이터 2글자명으로 통일

--- src/main/java/com/salayo/locallifebackend/global/init/CategoryDataInit.java
+++ src/main/java/com/salayo/locallifebackend/global/init/CategoryDataInit.java
@@ -21,7 +21,7 @@ public class CategoryDataInit {
         return args -> {
             String[] regionNames = {
                 "서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종",
-                "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주도"
+                "경도", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"
             };
 
             for (String name : regionNames) {
