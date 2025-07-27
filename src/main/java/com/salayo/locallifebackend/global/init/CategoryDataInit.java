ccde1a60d7ece29f38fb8c83ee4b73c789f5e7de
jiyoon <rlawldbs9128@gmail.com>
2025-07-28 02:26:39 +0900

    :wrench: 오타 수정

--- src/main/java/com/salayo/locallifebackend/global/init/CategoryDataInit.java
+++ src/main/java/com/salayo/locallifebackend/global/init/CategoryDataInit.java
@@ -21,7 +21,7 @@ public class CategoryDataInit {
         return args -> {
             String[] regionNames = {
                 "서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종",
-                "경도", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"
+                "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주"
             };
 
             for (String name : regionNames) {
