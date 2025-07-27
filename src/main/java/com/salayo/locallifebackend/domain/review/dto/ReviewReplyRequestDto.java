55e971cef9346cb7588e009a1ae0eefcf060a757
sooyeon_heo <suzi97@naver.com>
2025-07-28 05:18:21 +0900

    ✨ feat : 리뷰 요청 DTO 구현
    - ReviewRequestDto 구현
      - 리뷰 작성/수정 요청 구조
    
    - ReviewReplyRequestDto 구현
      - 답글 작성 요청 구조
    
    #122 #123 #124

---
+++ src/main/java/com/salayo/locallifebackend/domain/review/dto/ReviewReplyRequestDto.java
@@ -0,0 +1,10 @@
+package com.salayo.locallifebackend.domain.review.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReviewReplyRequestDto {
+	private String content;
+}
---
+++ src/main/java/com/salayo/locallifebackend/domain/review/dto/ReviewRequestDto.java
@@ -0,0 +1,10 @@
+package com.salayo.locallifebackend.domain.review.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReviewRequestDto {
+	private String content;
+}
