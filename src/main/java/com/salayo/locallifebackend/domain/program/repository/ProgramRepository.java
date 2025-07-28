ddb991a8669e96cb28e7f1eb0ae54d4ec2552b53
j-hann <j.han77055@gmail.com>
2025-07-28 12:31:13 +0900

    🔧  add : ProgramRepository & ReservationRepository 기본 메서드 추가
    - Service에서 매번 에외처리 안하도록 Repository에 공통 메서드 정의

--- src/main/java/com/salayo/locallifebackend/domain/program/repository/ProgramRepository.java
+++ src/main/java/com/salayo/locallifebackend/domain/program/repository/ProgramRepository.java
@@ -1,10 +1,16 @@
 package com.salayo.locallifebackend.domain.program.repository;
 
 import com.salayo.locallifebackend.domain.program.entity.Program;
+import com.salayo.locallifebackend.global.error.ErrorCode;
+import com.salayo.locallifebackend.global.error.exception.CustomException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProgramRepository extends JpaRepository<Program, Long>, ProgramRepositoryCustom{
+
+	default Program findByIdOrElseThrow(long programId){
+		return findById(programId).orElseThrow(() -> new CustomException(ErrorCode.PROGRAM_NOT_FOUND));
+	}
 
 }
