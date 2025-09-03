dbd8ea114a4e3348acfb887ed9237be8fbe38b01
jiyoon <rlawldbs9128@gmail.com>
2025-09-04 02:00:19 +0900

    :sparkles: feat: MagazineFeedbackCreateRequestDto에 내용 길이 제한 추가 -@Size(max = 1000) 어노테이션 추가하여 피드백 내용 최대 1000자 제한 -과도한 입력에 대한 방어 및 DB 부하 방지 목적

--- src/main/java/com/salayo/locallifebackend/domain/magazine/feedback/dto/MagazineFeedbackCreateRequestDto.java
+++ src/main/java/com/salayo/locallifebackend/domain/magazine/feedback/dto/MagazineFeedbackCreateRequestDto.java
@@ -1,6 +1,7 @@
 package com.salayo.locallifebackend.domain.magazine.feedback.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MagazineFeedbackCreateRequestDto {
 
+    @Size(max = 1000, message = "피드백 내용은 1000자 이내여야 합니다.")
     @NotBlank(message = "피드백 내용은 필수입니다.")
     private String content;
 
