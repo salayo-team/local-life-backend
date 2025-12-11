3e4a78d319599cda9f2e57e2f1676c103e9c3f67
sooyeon_heo <suzi97@naver.com>
2025-12-11 18:34:45 +0900

    ✏️ typos : String content의 컬럼명 각각 review_content, reply_content 명시화

--- src/main/java/com/salayo/locallifebackend/domain/review/entity/ReviewReply.java
+++ src/main/java/com/salayo/locallifebackend/domain/review/entity/ReviewReply.java
@@ -36,7 +36,7 @@ public class ReviewReply extends SoftDeletableEntity {
 	@JoinColumn(name = "local_creator_id", nullable = false)
 	private Member member;
 
-	@Column(name = "content", nullable = false)
+	@Column(name = "reply_content", nullable = false)
 	private String content;
 
 	@Builder
